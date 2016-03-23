6fe1152524bc834f197a667c6988c2791739bfe8
Luke Graybill <lgraybill@izeni.com>
2016-03-23 12:50:33 -0600

    Revert "Removes version tag"
    This reverts commit 227aa8e276ad087629faa644c2b9d341b5676511.

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='izeni-django-accounts',
+    version='1.1',
     namespace_packages=['izeni', 'izeni.django'],
     packages=find_packages(),
     include_package_data=True,
