0456e3b9baf2d1cf685ba20507fc6d326cdfad66
Justin Calder <jcalder@izeni.com>
2016-03-24 17:54:59 -0600

    updates permissions

--- izeni/django/accounts/permissions.py
+++ izeni/django/accounts/permissions.py
@@ -1,11 +1,17 @@
+from rest_framework import permissions
 from rest_framework.permissions import BasePermission
 
 
-# TODO: THIS IS WRONG!!!
-# TODO: Instead, we need to allow
-# * anonymous users to create
-# * logged in users to view
-# * only self or superuser to update/delete
 class BaseUserPermission(BasePermission):
     def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
         return request.user.is_authenticated() or request.method == 'POST'
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
+        if request.method not in permissions.SAFE_METHODS:
+            return obj.id == request.user.id
+        else:
+            return True
